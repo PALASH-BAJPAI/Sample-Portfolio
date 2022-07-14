9f38fda5aaa1be2e81e77d2034eb8a0f1c32bf00
PALASH-BAJPAI <palashbajpai45@gmail.com>
2022-07-14 18:15:30 +0530

    added html version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import HeroPage from "./HeroPage";
-
+import AboutPage from "./AboutPage";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App(){
     return(
         <div className="app">
             <HeroPage />
+            <AboutPage />
+            <Content />
+            <Footer />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css'
-import Model from "./Model";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
- <App />
+    <React.StrictMode>
+            <App />
+    </React.StrictMode>
 );
