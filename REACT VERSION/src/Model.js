9a0dfd21362e349eb7c6f86a584f07ea8561da1d
PALASH-BAJPAI <palashbajpai45@gmail.com>
2022-07-18 12:57:30 +0530

    html version added

--- REACT VERSION/src/Model.js
+++
@@ -1,8 +0,0 @@
-import Spline from '@splinetool/react-spline';
-export default function App() {
-  return (
-    <div className="model">
-    <Spline scene="https://prod.spline.design/HDFxPLmd0vUlOwrg/scene.splinecode" style={{"margin":"0"}}/>
-    </div>
-  );
-}
